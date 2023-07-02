e8785cb2eaee87e41956e21c5488e9e4af211166
Alison Carlos Santos <alison-carlos@hotmail.com>
2023-07-02 13:27:10 -0300

    Improved some features of the game

--- scripts/hangman.py
+++ scripts/hangman.py
@@ -1,4 +1,5 @@
 # Import
+import re
 import random
 from art import *
 from words import *
@@ -15,7 +16,7 @@ class Hangman():
         self.game_is_running = True
         self.reveal_word = ""
 
-        print(f"[DEBUG] - Selected word by the system was: {self.chosen_word}")
+        #print(f"[DEBUG] - Selected word by the system was: {self.chosen_word}")
         
     #Method to hide the letter on the board
     def __hide_word(self, word):
@@ -42,10 +43,7 @@ class Hangman():
     #Method to get player input
     def __input_letter(self) -> str:
         self.inputed_letter = str(input("Please, type a letter: "))
-        if self.inputed_letter.isalpha():
-            return self.inputed_letter 
-        else:
-            raise ValueError("The input is not a letter!")
+        return self.inputed_letter 
 
 
     #Method to guess the letter
@@ -53,22 +51,21 @@ class Hangman():
 
         self.inputed_letter = inputed_letter
         
-        print(f"[DEBUG] - Selected letter by the user: {inputed_letter}")
+        #print(f"[DEBUG] - Selected letter by the user: {inputed_letter}")
 
         if inputed_letter in self.chosen_word:
             print("Nice! you are right! 🎉")
 
             # Revials in the board the letters
             reveal_word = self.__reveal_letters(self.hidden_word, self.original_chosen_word, self.inputed_letter)
-            print(reveal_word)
-            
+            print(f"{reveal_word} \n")
+
             # Removes from chosen word the letter what player got right.
             self.chosen_word = self.chosen_word.replace(inputed_letter, "") 
 
         else:
             print("Sorry, you made an mistake in that attempt.")
             self.points -= 1
-        
 
         return self.points
 
@@ -83,9 +80,16 @@ class Hangman():
         print(f"Try to guess the word: {self.hidden_word}")
 
         while self.game_is_running:
-            print(f"[DEBUG] - Quantidade de pontos: {self.points}")
+            #print(f"[DEBUG] - Quantidade de pontos: {self.points}")
 
-            inputed_letter = self.__input_letter()
+
+            self.inputed_letter_is_valid = False
+            while self.inputed_letter_is_valid == False:
+                self.inputed_letter = self.__input_letter()
+                self.inputed_letter_is_valid = True if self.inputed_letter.isalpha() == True else False
+                print(f"The input {self.inputed_letter} is not a valid letter, try again.") if self.inputed_letter_is_valid == False else None
+
+
             self.points = self.__guess_letter(self.inputed_letter)
 
             print(board[6 - self.points])
@@ -93,6 +97,7 @@ class Hangman():
             if self.points == 0:
                 print("You lose! :(")
                 self.game_is_running = False
+                print(f"The word was: {self.original_chosen_word}")
             elif self.chosen_word == "":
                 print("Congratulations! You won! 🎉")
                 self.game_is_running = False
